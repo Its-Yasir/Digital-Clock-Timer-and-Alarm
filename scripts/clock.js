768f2d95358cb3ea094c61313a84c4e2e441cb69
Its-Yasir <iamyasirtariq@gmail.com>
2025-08-03 08:22:41 +0500

    Install Cursor

--- scripts/clock.js
+++ scripts/clock.js
@@ -7,6 +7,7 @@ export function updateClock(){
   let seconds = document.getElementById('seconds');
   let ampm = document.getElementById('ampm');
 
+  
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
